eb589a766e52c89ff69742dec5f522b81ce566bc
Livache515 <tanasecliviu@yahoo.com>
2019-12-20 12:48:05 +0200

    curs

---
+++ teme javascript/tema 1/tema1.js
@@ -0,0 +1,16 @@
+var aged=document.getElementById("container");
+   
+if (aged<6) {
+    alert("Generation Alpha");
+}else if (aged<=20 ) {
+    alert("GenerationZ");
+}else if (aged<=35) {
+    alert("GenerationY");
+}else if (aged<=50) {
+    alert("GenerationX"); 
+}else if (aged<=69) {
+    alert("Generation Baby Boomers");
+}else if (aged>=70) {
+    alert("Builders")
+}
+
